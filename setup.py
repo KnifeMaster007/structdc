a8c0e2cbc4f71c4ae832ff8eb34e76ba2fdc9a2d
KnifeMaster007 <km@j4u.su>
2019-03-26 01:40:18 +0300

    Added packaging info

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='structdc',
+    version='0.0.1',
+    packages=['structdc'],
+    url='https://github.com/KnifeMaster007/structdc',
+    license='MIT',
+    author='Ilia Galkin',
+    author_email='km@j4u.su',
+    python_requires='>=3.7',
+    description=''
+)
